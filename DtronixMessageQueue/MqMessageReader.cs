fa5b99f6f115732cecd467a6dee3690c2c12ed01
DJGosnell <DJGosnell@gmail.com>
2016-08-09 23:12:57 -0400

    Begun work on message reader-writer to ease end usage of the messages.

---
+++ DtronixMessageQueue/MqMessageReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtronixMessageQueue {
+	public class MqMessageReader {
+		private int position;
+		private MqMessage message;
+
+		public int Position {
+			get { return position; }
+			set { position = value; }
+		}
+
+		public MqMessage Message {
+			get { return message; }
+			set { message = value; }
+		}
+
+		public MqMessageReader(MqMessage message) {
+			this.message = message;
+		}
+	}
+}
