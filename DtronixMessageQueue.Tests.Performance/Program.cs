224abaa7c0c3d0fe969914f37deeae3bdd075909
DJGosnell <DJGosnell@gmail.com>
2016-08-17 20:37:25 -0400

    Completed first iteration of performance testing for multiple clients.

--- DtronixMessageQueue.Tests.Performance/Program.cs
+++ DtronixMessageQueue.Tests.Performance/Program.cs
@@ -36,9 +36,9 @@ namespace DtronixMessageQueue.Tests.Performance {
 				new MqFrame(RandomBytes(50), MqFrameType.Last)
 			};
 
-			MqPerformanceTests(10000, 1, small_message);
+			MqMultiClientPerformanceTests(10000, 2, small_message);
 
-			MqPerformanceTests(1000000, 5, small_message);
+			//MqPerformanceTests(1000000, 5, small_message);
 
 			var medimum_message = new MqMessage {
 				new MqFrame(RandomBytes(500), MqFrameType.More),
@@ -47,7 +47,7 @@ namespace DtronixMessageQueue.Tests.Performance {
 				new MqFrame(RandomBytes(500), MqFrameType.Last)
 			};
 
-			MqPerformanceTests(100000, 5, medimum_message);
+			//MqPerformanceTests(100000, 5, medimum_message);
 
 			var large_message = new MqMessage {
 				new MqFrame(RandomBytes(15000), MqFrameType.More),
@@ -56,7 +56,7 @@ namespace DtronixMessageQueue.Tests.Performance {
 				new MqFrame(RandomBytes(15000), MqFrameType.Last)
 			};
 
-			MqPerformanceTests(10000, 5, large_message);
+			//MqPerformanceTests(10000, 5, large_message);
 
 			Console.WriteLine("Performance complete");
 
@@ -89,11 +89,9 @@ namespace DtronixMessageQueue.Tests.Performance {
 				client_infos.Add(session, current_info);
 
 				connection_reset.Set();
-
-				Console.Write(client_infos.Count + " ");
 			};
 
-			Console.Write("Clients connecting to server:");
+			Console.WriteLine("Clients connecting to server");
 			for (int i = 0; i < clients; i++) {
 				var reset = new ManualResetEventSlim();
 				var cl = new MqClient();
@@ -106,6 +104,20 @@ namespace DtronixMessageQueue.Tests.Performance {
 
 				};
 
+				cl.Connected += (sender, args) => {
+					Task.Run(() => {
+						var client_run_info = current_info;
+						client_run_info.Stopwatch.Restart();
+						for (var j = 0; j < runs; j++) {
+							cl.Send(message);
+						}
+						MqServer sv = server;
+						client_run_info.ManualResetEventSlim.Wait();
+
+						client_run_info.Client.Close().Wait();
+					});
+				};
+
 				
 				cl.ConnectAsync("127.0.0.1").Wait();
 
@@ -113,7 +125,7 @@ namespace DtronixMessageQueue.Tests.Performance {
 
 			}
 
-			Console.WriteLine("\r\nClients connected to server  Starting tests.");
+			Console.WriteLine($"{client_infos.Count} clients connected to server  Starting tests.");
 
 			Console.WriteLine("|   Build |   Messages | Msg Bytes | Milliseconds |        MPS |     MBps |");
 			Console.WriteLine("|---------|------------|-----------|--------------|------------|----------|");
@@ -137,40 +149,25 @@ namespace DtronixMessageQueue.Tests.Performance {
 					mode = "Debug";
 #endif
 
-					var messages_per_second = (int)((double)runs / sw.ElapsedMilliseconds * 1000);
+					var messages_per_second = (int)((double)runs / client_info.Stopwatch.ElapsedMilliseconds * 1000);
 					var msg_size_no_header = message_size - 12;
-					var mbps = runs * (double)(msg_size_no_header) / sw.ElapsedMilliseconds / 1000;
-					Console.WriteLine("| {0,7} | {1,10:N0} | {2,9:N0} | {3,12:N0} | {4,10:N0} | {5,8:N2} |", mode, runs, msg_size_no_header, sw.ElapsedMilliseconds, messages_per_second, mbps);
+					var mbps = runs * (double)(msg_size_no_header) / client_info.Stopwatch.ElapsedMilliseconds / 1000;
+					Console.WriteLine("| {0,7} | {1,10:N0} | {2,9:N0} | {3,12:N0} | {4,10:N0} | {5,8:N2} |", mode, runs, msg_size_no_header, client_info.Stopwatch.ElapsedMilliseconds, messages_per_second, mbps);
 
 					client_info.ManualResetEventSlim.Set();
 				}
 
 			};
 
-			
-			var client = new MqClient();
+
+			var resets = client_infos.Values.Select(info => info.ManualResetEventSlim.WaitHandle).ToArray();
 
 
-			foreach (var client_run_info in client_infos) {
-				Task.Run(() => {
-					client_run_info.Value.Stopwatch.Restart();
-					for (var i = 0; i < runs; i++) {
-						client.Send(message);
-					}
-					MqServer sv = server;
-					client_run_info.Value.ManualResetEventSlim.Wait();
-					client_run_info.Value.ManualResetEventSlim.Reset();
-				});
-			}
-
-			var resets = client_infos.Values.
-
-
-			ManualResetEvent.WaitAll()
+			WaitHandle.WaitAll(resets);
 
 			Console.WriteLine("Complete");
 			server.Stop();
-			client.Close().Wait();*/
+			
 
 		}
 
