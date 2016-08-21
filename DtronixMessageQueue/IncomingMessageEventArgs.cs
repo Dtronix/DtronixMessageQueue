6d86d97f6a4798a154ebe8e0122ea11a2ccb88fa
DJGosnell <DJGosnell@gmail.com>
2016-08-21 16:55:56 -0400

    Added client reference to the IncomingMessageEventArgs. Added documentation.

--- DtronixMessageQueue/IncomingMessageEventArgs.cs
+++ DtronixMessageQueue/IncomingMessageEventArgs.cs
@@ -5,13 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DtronixMessageQueue {
+
+	/// <summary>
+	/// Event args for when a new message has been processed and is ready for usage.
+	/// </summary>
 	public class IncomingMessageEventArgs : EventArgs {
+
+		/// <summary>
+		/// Reference to the mailbox with the new message.
+		/// </summary>
 		public MqMailbox Mailbox { get; set; }
+
+		/// <summary>
+		/// If this message is on the server, this will contain the reference to the connected session of the client.
+		/// </summary>
 		public MqSession Session { get; set; }
 
-		public IncomingMessageEventArgs(MqMailbox mailbox, MqSession session) {
+		/// <summary>
+		/// If this message on the client, this will contain the reference to the client.
+		/// </summary>
+		public MqClient Client { get; set; }
+
+		/// <summary>
+		/// Creates an instance of the event args.
+		/// </summary>
+		/// <param name="mailbox">Mailbox with the new message</param>
+		/// <param name="session">Server session.  Null if this is on the client.</param>
+		/// <param name="client">Client.  Null if this is on the server.</param>
+		public IncomingMessageEventArgs(MqMailbox mailbox, MqSession session, MqClient client) {
 			Mailbox = mailbox;
 			Session = session;
+			Client = client;
 		}
 	}
 }
