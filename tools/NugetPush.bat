dfad7574929383f485e3eda60a45a14eb0734d51
DJGosnell <DJGosnell@gmail.com>
2016-09-14 12:25:51 -0400

    Renamed Tools directory to tools.

---
+++ tools/NugetPush.bat
@@ -0,0 +1,2 @@
+..\..\..\packages\NuGet.CommandLine.3.4.3\tools\nuget.exe push *0.nupkg -Source https://www.nuget.org/api/v2/package
+..\..\..\packages\NuGet.CommandLine.3.4.3\tools\nuget.exe push *.symbols.nupkg -Source https://www.nuget.org/api/v2/package
