1bd14910b0782705ad2daa118894e5626821097a
DJGosnell <DJGosnell@gmail.com>
2016-09-14 16:12:49 -0400

    Tests with changeover to smart thread pool instead of workers.

--- src/DtronixMessageQueue/MqPostmaster.cs
+++ src/DtronixMessageQueue/MqPostmaster.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
+using Amib.Threading;
 
 namespace DtronixMessageQueue {
 	/// <summary>
@@ -10,12 +11,14 @@ namespace DtronixMessageQueue {
 	public class MqPostmaster<TSession> : IDisposable
 		where TSession : MqSession<TSession>, new() {
 
+		private SmartThreadPool thread_pool;
+
 		private class WorkerInfo {
+
 			public enum WorkerType {
 				Reader,
 				Writer
 			}
-
 			public BlockingCollection<MqSession<TSession>> Operations;
 			public ConcurrentDictionary<MqSession<TSession>, bool> OngoingOperations;
 			public WorkerType Type;
@@ -37,11 +40,6 @@ namespace DtronixMessageQueue {
 		private readonly BlockingCollection<MqSession<TSession>> write_operations = new BlockingCollection<MqSession<TSession>>();
 
 		/// <summary>
-		/// List of all write workers.
-		/// </summary>
-		private readonly List<MqWorker> write_workers = new List<MqWorker>();
-
-		/// <summary>
 		/// Dictionary to prevent multiple reads occurring on the same session concurrently.
 		/// </summary>
 		private readonly ConcurrentDictionary<MqSession<TSession>, bool> ongoing_read_operations = new ConcurrentDictionary<MqSession<TSession>, bool>();
@@ -52,24 +50,11 @@ namespace DtronixMessageQueue {
 		private readonly BlockingCollection<MqSession<TSession>> read_operations = new BlockingCollection<MqSession<TSession>>();
 
 		/// <summary>
-		/// List of all read workers.
-		/// </summary>
-		private readonly List<MqWorker> read_workers = new List<MqWorker>();
-
-		/// <summary>
-		/// Current number of all writer workers.
-		/// </summary>
-		public int TotalWriters => write_workers.Count;
-
-		/// <summary>
-		/// Current number of all reader workers.
-		/// </summary>
-		public int TotalReaders => read_workers.Count;
-
-		/// <summary>
 		/// Configurations for this socket.
 		/// </summary>
 		private readonly MqSocketConfig config;
+
+		private CancellationTokenSource cancellation_token_source = new CancellationTokenSource();
 
 		/// <summary>
 		/// Creates a new postmaster instance to handle reading and writing of all sessions.
@@ -78,60 +63,32 @@ namespace DtronixMessageQueue {
 			this.config = config;
 
 			// Add a supervisor to review when it is needed to increase or decrease the worker numbers.
-			supervisor = new MqWorker(SupervisorWork, "postmaster_supervisor");
+			//supervisor = new MqWorker(SupervisorWork, "postmaster_supervisor");
 
 			// Create one reader and one writer workers to start off with.
-			for (int i = 0; i < 2; i++) {
-				CreateReadWorker();
-				CreateWriteWorker();
-			}
-			supervisor.Start();
-		}
-
-		/// <summary>
-		/// Supervisor method to review status of all workers.
-		/// Spawns or removes threads depending on workload.
-		/// </summary>
-		/// <param name="worker">Worker to review.</param>
-		private void SupervisorWork(MqWorker worker) {
-			while (worker.Token.IsCancellationRequested == false) {
-				if (ProcessWorkers(read_workers, config.MaxReadWriteWorkers)) {
-					CreateReadWorker();
-				}
-
-				if (ProcessWorkers(write_workers, config.MaxReadWriteWorkers)) {
-					CreateWriteWorker();
-				}
-				Thread.Sleep(500);
-			}
-		}
+			
 
 
-		/// <summary>
-		/// Reviews the specified worker list (reader/writer) and determines if new workers are needed or not.
-		/// </summary>
-		/// <param name="worker_list">Worker list to review.</param>
-		/// <param name="max_workers">Maximum number of workers for this list.</param>
-		/// <returns></returns>
-		private bool ProcessWorkers(List<MqWorker> worker_list, int max_workers) {
-			MqWorker idle_worker = null;
-			foreach (var worker in worker_list) {
-				if (idle_worker == null && worker.IsIdling) {
-					idle_worker = worker;
-				}
+			//supervisor.Start();
 
-				// If this is not the idle worker and it has been idle for more than 60 seconds, close down this worker.
-				if (worker.IsIdling && worker != idle_worker && worker.AverageIdleTime > config.IdleWorkerTimeout) {
-					worker_list.Remove(worker);
-					worker.Stop();
-				}
-			}
+			thread_pool = new SmartThreadPool(config.IdleWorkerTimeout, config.MaxReadWriteWorkers);
 
-			if (max_workers != 0 && worker_list.Count >= max_workers) {
-				return false;
-			}
+			var writer_info = new WorkerInfo {
+				Type = WorkerInfo.WorkerType.Writer,
+				OngoingOperations = ongoing_write_operations,
+				Operations = write_operations
+			};
 
-			return idle_worker == null;
+			thread_pool.QueueWorkItem(ProcessReadWrite, writer_info, cancellation_token_source.Token);
+
+
+			var reader_info = new WorkerInfo {
+				Type = WorkerInfo.WorkerType.Reader,
+				OngoingOperations = ongoing_read_operations,
+				Operations = read_operations
+			};
+
+			thread_pool.QueueWorkItem(ProcessReadWrite, reader_info, cancellation_token_source.Token);
 		}
 
 		/// <summary>
@@ -154,42 +111,30 @@ namespace DtronixMessageQueue {
 		}
 
 
-		/// <summary>
-		/// Creates a worker writer.
-		/// </summary>
-		public void CreateWriteWorker() {
-			var worker_info = new WorkerInfo {
-				Type = WorkerInfo.WorkerType.Writer,
-				OngoingOperations = ongoing_write_operations,
-				Operations = write_operations
-			};
-
-			var worker = new MqWorker(wrk => ReadWriteWork(wrk, worker_info), "mq_write_worker_" + write_workers.Count);
-
-			worker.Start();
-
-			write_workers.Add(worker);
-		}
-
-		private void ReadWriteWork(MqWorker worker, WorkerInfo info) {
+		private void ProcessReadWrite(WorkerInfo info, CancellationToken token) {
 			MqSession<TSession> session = null;
-			bool out_session;
-			bool work_done = false;
+			var more_work = false;
 			try {
-				worker.StartIdle();
-				while (work_done || info.Operations.TryTake(out session, 60000, worker.Token)) {
-					worker.StartWork();
+				do {
+					// Only queue this item up one time per method call.
+					if (more_work == false) {
+						info.Operations.TryTake(out session, 60000, token);
+						thread_pool.QueueWorkItem(ProcessReadWrite, info, token);
+					}
 
 					if (info.Type == WorkerInfo.WorkerType.Writer ? session.ProcessOutbox() : session.ProcessIncomingQueue()) {
-						work_done = true;
+						more_work = true;
 						continue;
 					}
 
-					work_done = false;
-					info.OngoingOperations.TryRemove(session, out out_session);
+					more_work = false;
 
-					worker.StartIdle();
-				}
+					bool out_session;
+					info.OngoingOperations.TryRemove(session, out out_session);
+				} while (more_work);
+
+				thread_pool.QueueWorkItem(ProcessReadWrite, info, token);
+
 			} catch (ThreadAbortException) {
 			} catch (Exception) {
 				if (session != null) {
@@ -201,35 +146,13 @@ namespace DtronixMessageQueue {
 			}
 		}
 
-		/// <summary>
-		/// Creates a worker reader.
-		/// </summary>
-		public void CreateReadWorker() {
-			var worker_info = new WorkerInfo {
-				Type = WorkerInfo.WorkerType.Reader,
-				OngoingOperations = ongoing_read_operations,
-				Operations = read_operations
-			};
-
-			var worker = new MqWorker(wrk => ReadWriteWork(wrk, worker_info), "mq_read_worker_" + read_workers.Count);
-
-			worker.Start();
-
-			read_workers.Add(worker);
-		}
 
 		/// <summary>
 		/// Stops all workers and removes all associated resources.
 		/// </summary>
 		public void Dispose() {
-			supervisor.Dispose();
-			foreach (var write_worker in write_workers) {
-				write_worker.Dispose();
-			}
-
-			foreach (var read_worker in read_workers) {
-				read_worker.Dispose();
-			}
+			thread_pool.Cancel(true);
+			thread_pool.Dispose();
 		}
 	}
 }
